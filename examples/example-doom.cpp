1b95e273f43caa5aa57e7a438c4bbfe66d36f421
Lucas C. Villa Real <lucasvr@gmail.com>
2020-10-16 09:23:40 -0300

    New example that shows Doom playing through HDF5
    In this demo, the user-defined-function attaches to a shared memory
    segment exported by a modified version of Doom -- that shm segment
    holds both the game framebuffer and the color palette. The revamped
    `readh5` utility reads the "Palette" and the "Framebuffer" datasets
    from `example-doom` and then prints out each grid cell using Truecolor
    ANSI codes.
    
    The modified version of Chocolate Doom can be obtained here:
    https://github.com/lucasvr/chocolate-doom

---
+++ examples/example-doom.cpp
@@ -0,0 +1,80 @@
+/*
+ * Play Doom through a virtual HDF5 dataset!
+ * 
+ * This example relies on a modified Doom engine that shares the display
+ * over a System V shared memory segment. You can download it from
+ * https://github.com/lucasvr/chocolate-doom.
+ * 
+ * Each time the dataset is read we attach to the shared memory segment
+ * and retrieve the framebuffer and the palette. We then encode that data
+ * as a regular 2D dataset and let h5dump and our own readh5 tools render
+ * it nicely.
+ *
+ * To embed this dataset in an existing HDF5 file, run:
+ *
+ * $ make files
+ * $ hdf5-udf example-doom.h5 example-doom.cpp Framebuffer:320x200:uint8 Palette:7680:uint8
+ *
+ * Note that the Palette size is 10x bigger than it needs to be. Once issue
+ * https://github.com/lucasvr/hdf5-udf/issues/7 is resolved we can make it
+ * into an array of 768 elements.
+ *
+ * The output looks best when rendered by readh5 in a loop:
+ * $ while true; do readh5 example-doom.h5 Framebuffer Palette; done
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/select.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/shm.h>
+#include <sys/ipc.h>
+#include <string>
+
+typedef struct { uint8_t r; uint8_t g; uint8_t b; } pal_t;
+bool retrieveDisplay(uint8_t *display, ssize_t width, ssize_t height, pal_t *pal, ssize_t pal_size);
+
+extern "C" void dynamic_dataset()
+{
+    // We export two datasets. The first is where the video dump
+    // is captured ("Framebuffer"). The second holds the RGB values
+    // to be used when rendering the framebuffer ("Palette").
+    auto fb_data  = lib.getData<uint8_t>("Framebuffer");
+    auto fb_dims  = lib.getDims("Framebuffer");
+    auto pal      = lib.getData<uint8_t>("Palette");
+    auto pal_dims = lib.getDims("Palette");
+
+    memset(pal, 0, pal_dims[0]);
+    pal_t *ptr = (pal_t *) pal;
+    retrieveDisplay(fb_data, fb_dims[0], fb_dims[1], ptr, pal_dims[0]/10);
+}
+
+bool retrieveDisplay(uint8_t *display, ssize_t width, ssize_t height, pal_t *pal, ssize_t pal_size)
+{
+    ssize_t size = (3 * 256) + (width * height);
+    int shmid = shmget(666, size, IPC_CREAT|0644);
+    if (shmid < 0)
+    {
+        fprintf(stderr, "Failed to allocate shared memory segment: %s\n", strerror(errno));
+        return false;
+    }
+
+    void *mm = shmat(shmid, NULL, 0);
+    if (mm == (void *) -1)
+    {
+        fprintf(stderr, "Failed to attach to shared memory segment\n");
+        return false;
+    }
+
+    memcpy(pal, mm, pal_size);
+    memcpy(display, ((char *) mm) + pal_size, width*height);
+
+    shmdt(mm);
+    return true;
+}
--- examples/readh5.cpp
+++ examples/readh5.cpp
@@ -9,26 +9,36 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <wchar.h>
 #include <hdf5.h>
 #include <string>
 #include <cstring>
+
+typedef struct { uint8_t r; uint8_t g; uint8_t b; } pal_t;
 
 int main(int argc, char **argv)
 {
     if (argc < 3)
     {
-        fprintf(stdout, "Syntax: %s <file.h5> <dataset>\n", argv[0]);
+        fprintf(stdout, "Syntax: %s <file.h5> <dataset> [palette dataset]\n", argv[0]);
         return 1;
     }
 
     std::string hdf5_file = argv[1];
     std::string hdf5_dataset = argv[2];
+    std::string hdf5_palette = argc > 3 ? argv[3] : "";
+
+    // Enable printing UTF-8 characters
+    setlocale(LC_ALL, "en_US.UTF-8");
+
     hid_t file_id = H5Fopen(hdf5_file.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
     if (file_id < 0)
     {
         fprintf(stderr, "Failed to open file %s\n", hdf5_file.c_str());
         return 1;
     }
+
+    // Read dataset
     hid_t dataset_id = H5Dopen(file_id, hdf5_dataset.c_str(), H5P_DEFAULT);
     if (dataset_id < 0)
     {
@@ -37,39 +47,89 @@ int main(int argc, char **argv)
     }
 
     hid_t type_id = H5Dget_type(dataset_id);
-    if (! H5Tequal(type_id, H5T_NATIVE_INT))
-    {
-        fprintf(stderr, "We're only ready to deal with native INT data types, sorry!\n");
-        return 1;
-    }
-
-    hsize_t dims[2] = {0, 0};
     hid_t space_id = H5Dget_space(dataset_id);
+    size_t datatype_size = H5Tget_size(type_id);
     int ndims = H5Sget_simple_extent_ndims(space_id);
     if (ndims != 2)
     {
         fprintf(stderr, "Expected a dataset with 2 dimensions, got %d instead\n", ndims);
         return 1;
     }
+    hsize_t dims[2] = {0, 0};
     H5Sget_simple_extent_dims(space_id, dims, NULL);
 
-    uint32_t *rdata = new uint32_t[dims[0] * dims[1]];
-    memset(rdata, 0, sizeof(uint32_t) * dims[0] * dims[1]);
-    H5Dread(dataset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, rdata);
+    uint8_t *rdata = new uint8_t[dims[0] * dims[1] * datatype_size];
+    if (H5Tequal(type_id, H5T_NATIVE_INT))
+        H5Dread(dataset_id, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, H5P_DEFAULT, rdata);
+    else if (H5Tequal(type_id, H5T_NATIVE_UINT8))
+        H5Dread(dataset_id, H5T_NATIVE_UINT8, H5S_ALL, H5S_ALL, H5P_DEFAULT, rdata);
+    else
+    {
+        fprintf(stderr, "We're only ready to deal with INT/UINT8 data types, sorry!\n");
+        return 1;
+    }
 
-    // The Tux dataset needs to be stretched a little bit so it looks prettier
-    // on the console. It also looks better when rendered as solid blocks instead
+    // Read palette, if provided. We assume it's an uint8_t
+    uint8_t *pal = NULL;
+    hsize_t pal_dims = 0;
+    if (hdf5_palette.size())
+    {
+        hid_t palette_id = H5Dopen(file_id, hdf5_palette.c_str(), H5P_DEFAULT);
+        hid_t pal_space_id = H5Dget_space(palette_id);
+        int ndims = H5Sget_simple_extent_ndims(pal_space_id);
+        if (ndims != 1)
+        {
+            fprintf(stderr, "Expected a palette with 1 dimension, got %d instead\n", ndims);
+            return 1;
+        }
+        H5Sget_simple_extent_dims(pal_space_id, &pal_dims, NULL);
+
+        pal = new uint8_t[pal_dims];
+        memset(pal, 0, pal_dims);
+        H5Dread(palette_id, H5T_NATIVE_UINT8, H5S_ALL, H5S_ALL, H5P_DEFAULT, pal);
+    }
+
+    // Tux and Doom datasets needs to be stretched a little bit so they look prettier
+    // on the console. They also look better when rendered as special characters instead
     // of a collection of 0s and 1s.
     bool is_tux = hdf5_dataset.compare("Tux") == 0;
-    for (size_t i=0; i<dims[0]*dims[1]; ++i) {
+    bool is_doom = hdf5_dataset.compare("Framebuffer") == 0;
+
+    char line[8192];
+    char *cur = line, *end = line + sizeof(line);
+    memset(line, 0, sizeof(line));
+    for (size_t i=0; i<dims[0]*dims[1]; ++i)
+    {
         if (i && i%dims[0] == 0)
-            printf("\n");
+        {
+            printf("%s\n", line);
+            memset(line, 0, sizeof(line));
+            cur = line;
+        }
+
         if (is_tux)
-            printf("%s", rdata[i] == 1 ? "  " : "\xE2\x96\x92\xE2\x96\x92");
+        {
+            wchar_t shade = 0x2592;
+            int *rdata_int = (int *) rdata;
+            cur += snprintf(cur, end-cur, "%lc%lc", rdata_int[i] == 1 ? ' ' : shade, rdata_int[i] == 1 ? ' ' : shade);
+        }
+        else if (is_doom)
+        {
+            pal_t *pal_ptr = (pal_t *) pal;
+            uint8_t data = rdata[i];
+            if (pal_ptr)
+            {
+                // Render this pixel using Truecolor ANSI escape codes
+                pal_t *color = &pal_ptr[data];
+                cur += snprintf(cur, end-cur, "\033[48;2;%d;%d;%dm**\033[0m", color->r, color->g, color->b);
+            }
+            else
+                cur += snprintf(cur, end-cur, "%c%c", data, data);
+        }
         else
-            printf("%d", rdata[i]);
+            cur += snprintf(cur, end-cur, "%d", (int) rdata[i]);
     }
-    printf("\n");
+    printf("%s\n", line);
 
     return 0;
 }
