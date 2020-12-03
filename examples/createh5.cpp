f3be6462aa726cfb42fad1cbaa136ff44778f5e6
Lucas C. Villa Real <lucasvr@gmail.com>
2020-12-02 22:34:31 -0300

    createh5: support the creation of compound datasets
    The generated dataset features 3 members with native types (int and
    double) and a rather different on-disk layout. The compound layout
    has been picked from the HDF Group examples directory.

--- examples/createh5.cpp
+++ examples/createh5.cpp
@@ -7,19 +7,53 @@
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <hdf5.h>
 #include <string>
+
+int create_regular_dataset(hid_t file_id, int count);
+int create_compound_dataset(hid_t file_id, int count);
+
+std::string getOptionValue(int argc, char **argv, const char *option, const char *default_value)
+{
+    for (int i=1; i<argc; ++i)
+    {
+        char *start = strstr(argv[i], option);
+        if (start == argv[i] && strlen(argv[i]) == strlen(option))
+        {
+            // No-argument option (e.g., --compound). Return "1"
+            return "1";
+        }
+        else if (start == argv[i])
+        {
+            char *value = &start[strlen(option) + strlen("=")];
+            return std::string(value);
+        }
+    }
+    return std::string(default_value);
+}
 
 int main(int argc, char **argv)
 {
     if (argc < 2)
     {
-        fprintf(stdout, "Syntax: %s <file.h5> [number_of_datasets]\n", argv[0]);
+        fprintf(stdout, "Syntax: %s  <options>\n", argv[0]);
+        fprintf(stdout, "Available options are:\n"
+            "  --compound        Create a compound dataset with a predefined structure\n"
+            "  --count=N         Create this many datasets in the output file (default: 0)\n"
+            "  --out=FILE        Output file name (truncates FILE if it already exists)\n\n");
         return 1;
     }
 
-    std::string hdf5_file = argv[1];
-    int dataset_count = argc == 3 ? atoi(argv[2]) : 0;
+    bool compound = atoi(getOptionValue(argc, argv, "--compound", "0").c_str()) == 1;
+    int dataset_count = atoi(getOptionValue(argc, argv, "--count", "0").c_str());
+    std::string hdf5_file = getOptionValue(argc, argv, "--out", "");
+    if (hdf5_file.size() == 0)
+    {
+        fprintf(stderr, "Error: missing output file (--out=FILE)\n");
+        return 1;
+    }
+
     hid_t file_id = H5Fcreate(hdf5_file.c_str(), H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
     if (file_id < 0)
     {
@@ -27,41 +61,119 @@ int main(int argc, char **argv)
         return 1;
     }
 
+    int ret = 0;
     for (int count=1; count<=dataset_count; ++count)
     {
-        const int dim0 = 100, dim1 = 50;
-        int data[dim0][dim1];
-        for (int i=0; i<dim0; ++i)
-            for (int j=0; j<dim1; ++j)
-                data[i][j] = count * 10 * i + j;
-
-        char name[64];
-        snprintf(name, sizeof(name)-1, "Dataset%d", count);
-        hsize_t dims[2] = {dim0,dim1};
-        hid_t space_id = H5Screate_simple(2, dims, NULL);
-        if (space_id < 0)
-        {
-            fprintf(stderr, "Failed to create dataspace\n");
-            return 1;
-        }
-        hid_t dset_id = H5Dcreate(file_id, name, H5T_STD_I32LE, space_id,
-            H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
-        if (dset_id < 0)
-        {
-            fprintf(stderr, "Failed to create dataset\n");
-            return 1;
-        }
-        herr_t ret = H5Dwrite(dset_id, H5T_NATIVE_INT,
-            H5S_ALL, H5S_ALL, H5P_DEFAULT, data[0]);
-        if (ret < 0)
-        {
-            fprintf(stderr, "Error writing data to file\n");
-            return 1;
-        }
-        H5Dclose(dset_id);
-        H5Sclose(space_id);
+        ret = compound ?
+            create_compound_dataset(file_id, count) :
+            create_regular_dataset(file_id, count);
+        if (ret != 0)
+            break;
     }
 
     H5Fclose(file_id);
+    return ret;
+}
+
+int create_regular_dataset(hid_t file_id, int count)
+{
+    const int dim0 = 100, dim1 = 50;
+    int data[dim0][dim1];
+    for (int i=0; i<dim0; ++i)
+        for (int j=0; j<dim1; ++j)
+            data[i][j] = count * 10 * i + j;
+
+    char name[64];
+    snprintf(name, sizeof(name)-1, "Dataset%d", count);
+    hsize_t dims[2] = {dim0,dim1};
+    hid_t space_id = H5Screate_simple(2, dims, NULL);
+    if (space_id < 0)
+    {
+        fprintf(stderr, "Failed to create dataspace\n");
+        return 1;
+    }
+    hid_t dset_id = H5Dcreate(file_id, name, H5T_STD_I32LE, space_id,
+        H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+    if (dset_id < 0)
+    {
+        fprintf(stderr, "Failed to create dataset\n");
+        return 1;
+    }
+    herr_t ret = H5Dwrite(dset_id, H5T_NATIVE_INT,
+        H5S_ALL, H5S_ALL, H5P_DEFAULT, data[0]);
+    if (ret < 0)
+    {
+        fprintf(stderr, "Error writing data to file\n");
+        return 1;
+    }
+    H5Dclose(dset_id);
+    H5Sclose(space_id);
+    return 0;
+}
+
+int create_compound_dataset(hid_t file_id, int count)
+{
+    const int dim0 = 1000;
+
+    struct compound_t {
+        int     serial_no;
+        double  temperature;
+        double  pressure;
+    } data[dim0];
+
+    for (int i=0; i<dim0; ++i) {
+        data[i].serial_no = i;
+        data[i].temperature = dim0/(i+1);
+        data[i].pressure = dim0/((i+1)*2);
+    }
+
+    char name[64];
+    snprintf(name, sizeof(name)-1, "Dataset%d", count);
+    hsize_t dims[1] = {dim0};
+    hid_t space_id = H5Screate_simple(1, dims, NULL);
+    if (space_id < 0)
+    {
+        fprintf(stderr, "Failed to create dataspace\n");
+        return 1;
+    }
+
+    // The dataset layout is based on the example provided by the HDF Group at
+    // https://support.hdfgroup.org/ftp/HDF5/examples/examples-by-api/hdf5-examples/1_8/C/H5T/h5ex_t_cmpd.c
+
+    hid_t memtype_id = H5Tcreate(H5T_COMPOUND, sizeof(compound_t));
+    H5Tinsert(memtype_id, "Serial number", HOFFSET(compound_t, serial_no), H5T_NATIVE_INT);
+    H5Tinsert(memtype_id, "Temperature (F)", HOFFSET(compound_t, temperature), H5T_NATIVE_DOUBLE);
+    H5Tinsert(memtype_id, "Pressure (inHg)", HOFFSET(compound_t, pressure), H5T_NATIVE_DOUBLE);
+
+    // Use a different disk layout to exercise HDF5-UDF's ability to pad the compound structure.
+    // Note that the example at the URL above includes a 'char *' to a string element that we
+    // don't include here -- that's what the sizeof(hvl_t) serves for. It doesn't hurt to keep
+    // that sizeof() here; it only makes things a bit more complicated for HDF5-UDF, which is
+    // good! It should be able to handle those special cases just fine.
+
+    hid_t filetype_id = H5Tcreate(H5T_COMPOUND, 8 + sizeof(hvl_t) + 8 + 8);
+    H5Tinsert(filetype_id, "Serial number", 0, H5T_STD_I64LE);
+    H5Tinsert(filetype_id, "Temperature (F)", 8 + sizeof(hvl_t), H5T_IEEE_F64LE);
+    H5Tinsert(filetype_id, "Pressure (inHg)", 8 + sizeof(hvl_t) + 8, H5T_IEEE_F64LE);
+
+    // Create the compound dataset
+    hid_t dset_id = H5Dcreate(file_id, name, filetype_id, space_id,
+        H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+    if (dset_id < 0)
+    {
+        fprintf(stderr, "Failed to create dataset\n");
+        return 1;
+    }
+
+    herr_t ret = H5Dwrite(dset_id, memtype_id, H5S_ALL, H5S_ALL, H5P_DEFAULT, data);
+    if (ret < 0)
+    {
+        fprintf(stderr, "Error writing data to file\n");
+        return 1;
+    }
+    H5Tclose(filetype_id);
+    H5Tclose(memtype_id);
+    H5Dclose(dset_id);
+    H5Sclose(space_id);
     return 0;
 }
