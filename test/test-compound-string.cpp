cd06a4d4108ed3839dcd23f513128115be257e9c
Lucas C. Villa Real <lucasvr@gmail.com>
2020-12-21 11:53:54 -0300

    Tests: UDFs and outputs for compounds with string members
    Add new regression tests for UDFs that access elements of a compound
    that include a string member.

---
+++ test/test-compound-string.cpp
@@ -0,0 +1,17 @@
+// hdf5-udf <file.h5> test-compound-string.cpp Temperature.cpp:1000:double
+extern "C" void dynamic_dataset()
+{
+    auto compound = lib.getData<compound_dataset1>("Dataset1");
+    auto udf_data = lib.getData<double>("Temperature.cpp");
+    auto udf_dims = lib.getDims("Temperature.cpp");
+
+    for (size_t i=0; i<udf_dims[0]; ++i)
+    {
+        printf("serial: %d, location: %s, temperature: %f, pressure: %f\n",
+            compound[i].serial_number,
+            compound[i].location,
+            compound[i].temperature,
+            compound[i].pressure);
+        udf_data[i] = compound[i].temperature;
+    }
+}
