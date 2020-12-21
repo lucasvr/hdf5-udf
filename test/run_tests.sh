133e4180ff43eb8ff52883892e32d0db939e8eac
Lucas C. Villa Real <lucasvr@gmail.com>
2020-12-21 11:48:11 -0300

    Tests: new script to check for regressions
    The new `run_tests.sh` script runs a few different UDFs and compares
    their output against a reference set. The script allows the comparison
    of the output of `h5dump` as well as of `stdout` (in which case the UDF
    outputs elements read with printf). All backends are supported by this
    simple test framework.

---
+++ test/run_tests.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+
+# High-level regression test suite for HDF5-UDF.
+
+# Logging (GoboLinux style)
+colorGray="\033[1;30m"
+colorBoldBlue="\033[1;34m"
+colorBrown="\033[33m"
+colorYellow="\033[1;33m"
+colorBoldGreen="\033[1;32m"
+colorBoldRed="\033[1;31m"
+colorCyan="\033[36m"
+colorBoldCyan="\033[1;36m"
+colorRedWhite="\033[41;37m"
+colorNormal="\033[0m"
+colorBold="${colorYellow}"
+colorOff="${colorNormal}"
+
+function Log_Function() {
+    local message="$1"
+    local color="$2"
+    local testName="$3"
+    if [ "$testName" ]
+    then echo -e "${colorGray}${testName}:${colorNormal} ${color}$message${colorNormal}"
+    else echo -e "${color}$message${colorNormal}"
+    fi
+}
+
+function Die() {
+    local message="$1"
+    local test_info="$2"
+    Log_Function "$message" "${colorBoldRed}" "$test_info"
+    Log_Function "Please check $RESULT_STDOUT and $RESULT_H5DUMP for details" "${colorBoldRed}" "$test_info"
+    exit 1
+}
+
+function Log_Normal() {
+    local message="$1"
+    local test_info="$2"
+    Log_Function "$message" "${colorCyan}" "$test_info"
+}
+
+# Helpers
+function Read_Dataset() {
+    local dataset="$1"
+    local input_file="$2"
+    h5dump -d "$dataset" -o "$RESULT_H5DUMP" -O "$input_file"
+}
+
+function Run_Test() {
+    local entry="$1"
+    local test_name="$(echo $entry | awk {'print $1'})"
+    local file_name="$(echo $entry | awk {'print $2'})"
+    local params="$(echo $entry | awk {'print $3'})"
+    local var_name="$(echo $params | cut -d: -f1)"
+
+    if [ "$params" = "$var_name" ]
+    then
+        # Let hdf5-udf determine the dynamic dataset's name, resolution and data type
+        params=
+    fi
+
+    for backend in "cpp" "py" "lua"
+    do
+        [ ! -e "${file_name}.${backend}" ] && continue
+
+        # Adjust variable and dataset names so they're named after the backend being tested
+        dataset_name="${var_name}.${backend}"
+        if [ "$params" ]
+        then dataset_info="$dataset_name:$(echo $params | cut -d: -f2-)"
+        else dataset_info=
+        fi
+
+        test_info="${test_name} (${backend})"
+        Log_Normal "Writing UDF" "${test_info}"
+        hdf5-udf ${test_name}.h5 ${file_name}.${backend} ${dataset_info}
+
+        Log_Normal "Reading UDF" "${test_info}"
+        Read_Dataset /${dataset_name} ${test_name}.h5 > $RESULT_STDOUT
+        if [ -e ${test_name}.stdout.${backend} ]
+        then
+            diff -q $RESULT_STDOUT ${test_name}.stdout.${backend} || Die "Validation failed" "${test_info}"
+        elif [ -e ${test_name}.stdout ]
+        then
+            diff -q $RESULT_STDOUT ${test_name}.stdout || Die "Validation failed" "${test_info}"
+        fi
+        if [ -e ${test_name}.h5dump ]
+        then
+            diff -q $RESULT_H5DUMP ${test_name}.h5dump || Die "Validation failed" "${test_info}"
+        fi
+        rm -f $RESULT_STDOUT $RESULT_H5DUMP
+        Log_Normal "Successful validation" "${test_info}"
+    done
+    rm -f ${test_name}.h5
+}
+
+# Operation
+export HDF5_PLUGIN_PATH=../src
+export PATH=../src:$PATH
+RESULT_STDOUT=stdout.txt
+RESULT_H5DUMP=h5dump.txt
+
+make -C ../examples files
+cp -a ../examples/example-*.h5 .
+rename "example-" "" *.h5
+
+# Notes:
+# File names in the 'tests' array are given without their extension. The dynamic
+# dataset may be given by its name alone (in which case hdf5-udf will determine
+# its dimensions and data type) or the colon-separated name:resolution:type combo.
+#
+# To allow reuse of HDF5 files, each backend operates on dynamic datasets whose
+# names are suffixed after that backend. For instance, 'test.cpp' will work on a
+# dynamic dataset named 'VirtualDataset.cpp', 'test.lua' on 'VirtualDataset.lua'
+# and so on. When adding new entries to the 'tests' array, please omit that suffix;
+# the Run_Test() function will take care of putting it back when it's needed.
+
+Log_Normal
+Log_Normal "****************************"
+Log_Normal "*** Single dataset tests ***"
+Log_Normal "****************************"
+Log_Normal
+tests=(
+    # HDF5 file     UDF file       Dynamic dataset
+    "simple_vector  simple_vector  Simple:1500:float"
+    "sine_wave      sine_wave      SineWave:100x10:int32"
+)
+for entry in "${tests[@]}"; do Run_Test "$entry"; done
+
+Log_Normal
+Log_Normal "*******************************"
+Log_Normal "*** Multiple datasets tests ***"
+Log_Normal "*******************************"
+Log_Normal
+tests=(
+    # HDF5 file    UDF file             Dynamic dataset
+    "add_datasets  test-multi-datasets  VirtualDataset"
+)
+for entry in "${tests[@]}"; do Run_Test "$entry"; done
+
+Log_Normal
+Log_Normal "**********************"
+Log_Normal "*** Compound tests ***"
+Log_Normal "**********************"
+Log_Normal
+tests=(
+    # HDF5 file                UDF file                  Dynamic dataset
+    "compound-nostring_simple  test-compound-nostring    Temperature:1000:double"
+    "compound-nostring_mixed   test-compound-nostring    Temperature:1000:double"
+    "compound-varstring_simple test-compound-string      Temperature:1000:double"
+    "compound-varstring_mixed  test-compound-string      Temperature:1000:double"
+    "compound-string_simple    test-compound-string      Temperature:1000:double"
+    "compound-string_mixed     test-compound-string      Temperature:1000:double"
+)
+for entry in "${tests[@]}"; do Run_Test "$entry"; done
+
+rm -f -- *.h5
