c2f9e1c1d591403a41c80ab5fc6683b24e9fdebd
Lucas C. Villa Real <lucasvr@gmail.com>
2021-04-19 13:42:52 -0300

    Python: improve setup.py
    Add required files for proper packaging.

--- python-bindings/setup.py
+++ python-bindings/setup.py
@@ -2,25 +2,29 @@
 
 from setuptools import setup, find_packages
 
-long_description = """
-This module provides a Python interface to HDF5-UDF so that users can
-embed routines on HDF5 files programmatically.
-"""
-
 setup(
-    name="hdf5_udf",
+    name="hdf5-udf",
     version="1.0",
     description="User-defined functions for HDF5 - Python bindings",
-    long_description=long_description,
+    description_content_type="text/x-rst",
+    long_description=open("README.txt").read(),
+    long_description_content_type="text/x-rst",
     author="Lucas C. Villa Real",
+    author_email="lucasvr@gmail.com",
+    url="https://pyhdf5-udf.readthedocs.io",
 
     py_modules=["hdf5_udf"],
     python_requires=">=3.6",
-    setup_requires=["cffi>=1.0.0"],
+    setup_requires=[
+        "wheel",
+        "cffi>=1.0.0"
+    ],
     install_requires=[
         "cffi>=1.0.0",
         "jsonschema>=3.2.0"
     ],
+    packages=find_packages(),
+    include_package_data=True,
 
     cffi_modules=["build_ext.py:ffibuilder"],
     zip_safe=False,
